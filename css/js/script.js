40a7f33472770dcffc019ccfee3c3971e4399db3
dharadasadiya <dharadasadiya64@gmail.com>
2026-02-24 18:59:57 +0530

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,14 @@
-console.log("Day 1 Task Loaded");
+// Page loaded message
+console.log("Day 2 - Button Component System Loaded");
+
+// Select all buttons
+const buttons = document.querySelectorAll(".btn");
+
+// Add click event to each button
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    if (!button.disabled) {
+      console.log("Button clicked:", button.innerText);
+    }
+  });
+});
