4c27f015f89b4dfaf668dce8fa3998faf936d917
dharadasadiya <dharadasadiya64@gmail.com>
2026-02-24 19:28:30 +0530

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,14 +1,11 @@
-// Page loaded message
-console.log("Day 2 - Button Component System Loaded");
+console.log("Day 2 Loaded");
 
-// Select all buttons
 const buttons = document.querySelectorAll(".btn");
 
-// Add click event to each button
-buttons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    if (!button.disabled) {
-      console.log("Button clicked:", button.innerText);
+buttons.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    if (!btn.disabled) {
+      console.log(btn.innerText + " button clicked");
     }
   });
 });
