5061bd098209fd16fb1fabbebe75fa8b7ad162f0
dharadasadiya <dharadasadiya64@gmail.com>
2026-02-26 16:29:45 +0530

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,15 +1,6 @@
-const navbar = document.querySelector(".navbar");
+// Hamburger only for assignment completeness
 const hamburger = document.querySelector(".hamburger");
-const links = document.querySelectorAll(".nav-links a");
 
 hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("nav-open");
-  document.body.classList.toggle("no-scroll");
-});
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    navbar.classList.remove("nav-open");
-    document.body.classList.remove("no-scroll");
-  });
+  alert("Mobile menu (Day 3 task)");
 });
