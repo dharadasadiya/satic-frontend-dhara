48cb93e4c7f52831a7e86b2636019cc2833dea2d
dharadasadiya <dharadasadiya64@gmail.com>
2026-03-06 17:04:19 +0530

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -129,3 +129,23 @@ form.addEventListener("submit", function(e) {
   form.reset();
   submitBtn.disabled = true;
 });
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function(){
+
+if(window.scrollY > 200){
+topBtn.style.display = "block";
+}else{
+topBtn.style.display = "none";
+}
+
+});
+
+topBtn.addEventListener("click", function(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+});
